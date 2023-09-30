9058abf75d440287f57770f7fc0eb6407ce7c058
rickard <rickard@mindemia.com>
2023-09-30 08:17:04 +0200

    need to ask something after creating character

--- stories/anything/story.py
+++ stories/anything/story.py
@@ -1,14 +1,17 @@
 import pathlib
+import random
 import sys
 from typing import Generator
 
 import tale
+from tale import lang
 from tale.driver import Driver
 from tale.llm.llm_ext import DynamicStory
 from tale.main import run_from_cmdline
 from tale.player import Player, PlayerConnection
 from tale.charbuilder import PlayerNaming
 from tale.story import *
+from tale.weapon_type import WeaponType
 from tale.zone import Zone
 
 class Story(DynamicStory):
@@ -41,6 +44,11 @@ class Story(DynamicStory):
         Called by the game driver when it has created the player object (after successful login).
         You can set the hint texts on the player object, or change the state object, etc.
         """
+        player.stats.set_weapon_skill(weapon_type=WeaponType.ONE_HANDED_RANGED, value=random.randint(10, 30))
+        player.stats.set_weapon_skill(weapon_type=WeaponType.TWO_HANDED_RANGED, value=random.randint(10, 30))
+        player.stats.set_weapon_skill(weapon_type=WeaponType.ONE_HANDED, value=random.randint(10, 30))
+        player.stats.set_weapon_skill(weapon_type=WeaponType.TWO_HANDED, value=random.randint(10, 30))
+        player.stats.set_weapon_skill(weapon_type=WeaponType.UNARMED, value=random.randint(20, 30))
         pass
 
     def create_account_dialog(self, playerconnection: PlayerConnection, playernaming: PlayerNaming) -> Generator:
@@ -51,6 +59,13 @@ class Story(DynamicStory):
         Ask questions using the yield "input", "question?"  mechanism.
         Return True to declare all is well, and False to abort the player creation process.
         """
+        ranged = yield "input", ("Do you prefer ranged over close combat? (yes/no)", lang.yesno)
+        if ranged:
+            playerconnection.player.stats.set_weapon_skill(weapon_type=WeaponType.ONE_HANDED_RANGED, value=random.randint(20, 40))
+            playerconnection.player.stats.set_weapon_skill(weapon_type=WeaponType.TWO_HANDED_RANGED, value=random.randint(20, 40))
+        else:
+            playerconnection.player.stats.set_weapon_skill(weapon_type=WeaponType.ONE_HANDED, value=random.randint(20, 40))
+            playerconnection.player.stats.set_weapon_skill(weapon_type=WeaponType.TWO_HANDED, value=random.randint(20, 40))
         return True
 
     def welcome(self, player: Player) -> str:
