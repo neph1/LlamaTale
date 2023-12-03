7bb25b2e365b072e3835d38d244883b1c2a7f2ec
rickard <rickard@mindemia.com>
2023-12-03 13:49:33 +0100

    fix add_event test

--- tests/test_wizard_commands.py
+++ tests/test_wizard_commands.py
@@ -17,6 +17,7 @@ from tests.supportstuff import FakeDriver, FakeIoUtil
 class TestEnrichCommand():
     
     context = tale._MudContext()
+    tale.mud_context = context
     context.config = StoryConfig()
     io_util = FakeIoUtil(response=['{"items":[{"name":"Enchanted Petals", "type":"Health", "value": 20, "description": "A handful of enchanted petals that can be used to heal wounds and cure ailments."}]}',
                                    '{"creatures": [   {   "name": "Whimsy Woozle",   "description": "A gentle, ethereal creature with a penchant for gardening and poetry. They tend to the area\'s lush fields and forests, filling the air with sweet melodies and the scent of blooming wildflowers. They are friendly and welcoming to all visitors.",   "level": 1   },   {   "name": "Lunar Lopster",   "description": "A mysterious crustacean with an affinity for the moon\'s gentle light. They roam the area at night, their glowing shells lighting the way through the darkness. They are neutral towards visitors, but may offer cryptic advice or guidance to those who seek it.",   "level": 2   },   {   "name": "Shadow Stag",   "description": "A sleek and elusive creature with a mischievous grin. They roam the area\'s forests, their dark forms blending into the shadows. They are hostile towards intruders, and will not hesitate to attack those who threaten their home.",   "level": 3   },   {   "name": "Moonflower",   "description": "A rare and beautiful flower that blooms only under the light of the full moon. They can be found in the area\'s forests, and are said to have powerful healing properties. They are very friendly towards visitors, and will offer their petals to those who show kindness and respect.",   "level": 4   },   {   "name": "Moonstone",   "description": "A rare and valuable mineral that can be found in the area\'s mountains. It glows with a soft, ethereal light, and is said to have powerful magical properties. It is highly sought after by collectors, and can be found in both the earth and the water.",   "level": 5   }]}'])
@@ -55,6 +56,7 @@ class TestEnrichCommand():
 class TestEvents():
 
     context = tale._MudContext()
+    tale.mud_context = context
     context.config = StoryConfig()
     io_util = FakeIoUtil(response='')
     llm_util = LlmUtil(io_util)
