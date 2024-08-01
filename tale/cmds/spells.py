d7a868e84f1c2c1322670e5e76c210c1bbd4e52b
rickard <rickardeden@gmail.com>
2024-08-01 18:24:47 +0200

    more spells

--- tale/cmds/spells.py
+++ tale/cmds/spells.py
@@ -1,5 +1,6 @@
 
 import random
+from typing import Optional
 from tale import base, util, cmds
 from tale.cmds import cmd
 from tale.errors import ActionRefused, ParseError
@@ -14,6 +15,9 @@ def do_heal(player: Player, parsed: base.ParseResult, ctx: util.Context) -> None
     skill = player.stats.magic_skills.get(MagicType.HEAL) # type: MagicSkill
     if not skill:
         raise ActionRefused("You don't know how to heal")
+
+    if not skill.spell.check_cost(player, level):
+        raise ActionRefused("You don't have enough magic points")
     
     num_args = len(parsed.args)
     if num_args < 1:
@@ -23,16 +27,13 @@ def do_heal(player: Player, parsed: base.ParseResult, ctx: util.Context) -> None
     except ValueError as x:
         raise ActionRefused(str(x))
     
-    result = player.location.search_living(entity) # type: base.Living
+    result = player.location.search_living(entity) # type: Optional[base.Living]
     if not result or not isinstance(result, base.Living):
         raise ActionRefused("Can't heal that")
     
     level = player.stats.level
     if num_args == 2:
         level = int(parsed.args[1])
-
-    if player.stats.magic_points < skill.spell.base_cost * level:
-        raise ActionRefused("You don't have enough magic points")
     
     player.stats.magic_points -= skill.spell.base_cost * level
 
@@ -41,6 +42,8 @@ def do_heal(player: Player, parsed: base.ParseResult, ctx: util.Context) -> None
         return
 
     result.stats.replenish_hp(5 * level)
+    player.tell("You heal %s for %d hit points" % (result.name, 5 * level), evoke=True)
+    player.tell_others("%s heals %s" % (player.name, result.name), evoke=True)
 
 
 @cmd("bolt")
@@ -51,6 +54,9 @@ def do_bolt(player: Player, parsed: base.ParseResult, ctx: util.Context) -> None
     if not skill:
         raise ActionRefused("You don't know how to cast a bolt")
     
+    if not skill.spell.check_cost(player, level):
+        raise ActionRefused("You don't have enough magic points")
+    
     num_args = len(parsed.args)
     if num_args < 1:
         raise ParseError("You need to specify who or what to attack")
@@ -60,7 +66,7 @@ def do_bolt(player: Player, parsed: base.ParseResult, ctx: util.Context) -> None
     except ValueError as x:
         raise ActionRefused(str(x))
     
-    result = player.location.search_living(entity) # type: base.Living
+    result = player.location.search_living(entity) # type: Optional[base.Living]
     if not result or not isinstance(result, base.Living):
         raise ActionRefused("Can't attack that")
     
@@ -68,13 +74,60 @@ def do_bolt(player: Player, parsed: base.ParseResult, ctx: util.Context) -> None
     if num_args == 2:
         level = int(parsed.args[1])
 
-    if player.stats.magic_points < skill.spell.base_cost * level:
-        raise ActionRefused("You don't have enough magic points")
     
     player.stats.magic_points -= skill.spell.base_cost * level
 
     if random.randint(1, 100) > skill.skill:
-        player.tell("Your healing spell fizzles out", evoke=True)
+        player.tell("Your bolt spell fizzles out", evoke=True)
         return
 
-    result.stats.hp -= random.randrange(1, level)
+    hp = random.randrange(1, level)
+    result.stats.hp -= hp
+    player.tell("Your energy bolt hits %s for %d damage" % (result.name, hp), evoke=True)
+    player.tell_others("%s's energy bolt hits %s for %d damage" % (player.name, result.name, hp), evoke=True)
+
+@cmd("drain")
+def do_drain(player: Player, parsed: base.ParseResult, ctx: util.Context) -> None:
+    """ Drain energy from someone or something """
+
+    skill = player.stats.magic_skills.get(MagicType.DRAIN)
+    if not skill:
+        raise ActionRefused("You don't know how to drain")
+    
+    if not skill.spell.check_cost(player, level):
+        raise ActionRefused("You don't have enough magic points")
+    
+    num_args = len(parsed.args)
+    if num_args < 1:
+        raise ParseError("You need to specify who or what to drain")
+    
+    try:
+        entity = str(parsed.args[0])
+    except ValueError as x:
+        raise ActionRefused(str(x))
+    
+    result = player.location.search_living(entity) # type: Optional[base.Living]
+    if not result or not isinstance(result, base.Living):
+        raise ActionRefused("Can't drain that")
+    
+    level = player.stats.level
+    if num_args == 2:
+        level = int(parsed.args[1])
+    
+    
+    player.stats.magic_points -= skill.spell.base_cost * level
+
+    if random.randint(1, 100) > skill.skill:
+        player.tell("Your drain spell fizzles out", evoke=True)
+        return
+    
+    points = random.randrange(1, level)
+    result.stats.combat_points -= points
+    result.stats.magic_points -= points
+
+    player.stats.magic_points += points
+
+    player.tell("Your drain spell drains %s of %d combat and magic points" % (result.name, points), evoke=True)
+    player.tell_others("%s casts a drain spell that drains energy from %s" % (player.name, result.name), evoke=True)
+    
+
--- tale/magic.py
+++ tale/magic.py
@@ -1,9 +1,14 @@
 from abc import ABC
 from enum import Enum
 
+from tale.player import Player
+
 
 class MagicType(Enum):
     HEAL = 1
+    BOLT = 2
+    DRAIN = 3
+    REJUVENATE = 4
 
 
 
@@ -17,8 +22,14 @@ class Spell(ABC):
     def do_cast(self, player, target, level):
         pass
 
+    def check_cost(self, player: Player, level: int) -> bool:
+        return player.stats.magic_points >= self.base_cost * level
+
 spells = {
-    MagicType.HEAL: Spell('heal', base_cost=2, base_value=5)
+    MagicType.HEAL: Spell('heal', base_cost=2, base_value=5),
+    MagicType.BOLT: Spell('bolt', base_cost=3, base_value=5),
+    MagicType.DRAIN: Spell('drain', base_cost=3, base_value=5),
+    MagicType.REJUVENATE: Spell('rejuvenate', base_cost=3, base_value=5)
 }
 
 class MagicSkill:
